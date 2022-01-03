088248d150f535bfaaa6b9f9e6d840ac5b06533e
Bett-C <collins.kkiprotich.student@moringaschool.com>
2022-01-03 19:24:37 +0300

    it will add jquery on js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,91 @@
 $(document).ready(function () {
     $("#design-p").click(function () {
-        $("#design").show();
-        $("#design-p").hide();
+        $("#design").show()
+        $("#design-p").hide()
 
 
-    });
+    })
+    $("#development").click(function () {
+        $("#develop").show()
+        $("#devopment").hide()
+
+    })
+
+    $("#product-design").click(function () {
+        $("#product").show()
+        $("#product-design").hide()
+    })
+    $(".one").mouseenter(function () {
+        $("#one").show()
+    })
+
+    $(".one").mouseenter(function () {
+        $("#one").hide()
+
+    })
+
+    $(".two").mouseenter(function () {
+        $("#two").show()
+
+    })
+
+    $(".two").mouseenter(function () {
+        $("#two").hide()
+
+    })
+    $(".three").mouseenter(function () {
+        $("#three").show()
+
+    })
+
+    $(".three").mouseenter(function () {
+        $("#three").hide()
+
+    })
+
+    $(".four").mouseenter(function () {
+        $("#four").show()
+
+    })
+
+    $(".four").mouseenter(function () {
+        $("#four").hide()
+
+    })
+    $(".five").mouseenter(function () {
+        $("#five").show()
+
+    })
+
+    $(".five").mouseenter(function () {
+        $("#five").hide()
+
+    })
+    $(".six").mouseenter(function () {
+        $("#six").show()
+
+    })
+
+    $(".six").mouseenter(function () {
+        $("#six").hide()
+
+    })
+    $(".seven").mouseenter(function () {
+        $("#seven").show()
+
+    })
+
+    $(".seven").mouseenter(function () {
+        $("#seven").hide()
+
+    })
+    $(".eight").mouseenter(function () {
+        $("#eight").show();
+
+    })
+
+    $(".eight").mouseenter(function () {
+        $("#eight").hide()
+
+    })
 })
